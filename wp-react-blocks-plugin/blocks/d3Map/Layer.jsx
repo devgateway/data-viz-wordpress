484b541da11f8f71b9a4bdc25af6cb3aee69ada1
Sebastian Dimunzio <sdimunzio@gmail.com>
2023-05-15 18:11:54 -0300

    adding new map

---
+++ wp-react-blocks-plugin/blocks/d3Map/Layer.jsx
@@ -0,0 +1,22 @@
+import {PanelBody, PanelRow} from "@wordpress/components";
+
+export const layerPrototype     = {
+    name:'New Layer',
+    file:'',
+    type:'Shape',
+    color:'#000000',
+    opacity:1,
+}
+
+const LayerSettings =(layer)=> {
+
+    const {name,type,color,opacity} = layer
+
+    return <PanelBody title={name}>
+                <PanelRow>
+                    <label>Layer Name</label>
+                </PanelRow>
+            </PanelBody>
+}
+
+export default LayerSettings
---
+++ wp-react-blocks-plugin/blocks/d3Map/index.js
@@ -0,0 +1,60 @@
+import {__} from '@wordpress/i18n';
+import {registerBlockType} from '@wordpress/blocks';
+import BlockSave from "./BlockSave";
+import BlockEdit from "./BlockEdit";
+import Generic from "../icons";
+
+registerBlockType(process.env.BLOCKS_NS+'/new-map',
+    {
+        title: __('D3 Map'),
+        icon: Generic,
+        category: process.env.BLOCKS_CATEGORY,
+        apiVersion: 2,
+        attributes: {
+            height: {
+                type: 'Numeric',
+                default: 500
+            },
+            group: {
+                type: 'string',
+                default: 'default',
+            },
+
+            app: {
+                type: 'string',
+                default: 'csv',
+            },
+
+            dimension1: {
+                type: 'String',
+                default: 'zone'
+            },
+            dimension2: {
+                type: 'String',
+                default: 'gender'
+            },
+
+            measures: {
+                type: "Object",
+                default: {}
+            },
+
+            csv: {
+                type: "String",
+                default: ""
+            },
+            layers: {
+                type: "Array",
+                default: []
+            },
+            panelStatus: {
+                type: "Object",
+                default: {}
+            },
+
+
+        },
+        edit: BlockEdit,
+        save: BlockSave,
+    }
+);
