5a352ce791157f94331200ce7268a3d0e1b5b178
Sebastian Dimunzio <sdimunzio@gmail.com>
2023-05-19 08:58:01 -0300

    adding d3Map

--- wp-react-blocks-plugin/blocks/d3Map/FileUtils.js
+++ wp-react-blocks-plugin/blocks/d3Map/FileUtils.js
@@ -1,21 +1,6 @@
-const queryJsonFiles = ({
-                            fileType,
-                            taxonomy
-                        }) => {
-
-    const fileTypeFilter = taxonomy != 'none' && fileType != 'none' ? `&${taxonomy}=${fileType}` : ''
-    const mapFiles = [{value: '', label: 'None'}]
-    wp.apiFetch({
-        path: '/wp/v2/media?mime_type=application/json' + fileTypeFilter,
-    }).then(json => {
-        if (json) {
-            json.forEach(f => {
-                mapFiles.push({value: f.id, label: f.title.rendered})
-            })
-
-            this.setState({mapFiles: mapFiles})
-        }
-    });
+export const getJsonFiles = () => {
+    return wp.apiFetch({path: '/wp/v2/media?mime_type=application/json'});
 }
 
-export default queryJsonFiles
+
+export default this
