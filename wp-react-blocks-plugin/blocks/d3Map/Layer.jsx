760c4726dd05e92c54cd7eded3fe19ed0bf4f2a3
Sebastian Dimunzio <sdimunzio@gmail.com>
2023-06-01 08:28:20 -0300

    adding layers,labels, colors and zoom on d3map

--- wp-react-blocks-plugin/blocks/d3Map/Layer.jsx
+++ wp-react-blocks-plugin/blocks/d3Map/Layer.jsx
@@ -1,13 +1,23 @@
-import {Button, PanelBody, PanelRow, SelectControl, TextControl} from "@wordpress/components";
+import {
+    Button,
+    PanelBody,
+    PanelRow,
+    RangeControl,
+    SelectControl,
+    TextControl,
+    ToggleControl
+} from "@wordpress/components";
 import {__} from '@wordpress/i18n';
 import {getJsonFiles} from "./FileUtils";
 import {useEffect} from "react";
 import {useState} from "@wordpress/element";
 import ***REMOVED*** from "./***REMOVED***";
 import {BlockEditWithAPIMetadata, ComponentWithSettings} from "../commons";
+import Property from "./Property";
+import {FieldSet} from "./LayerObject"
+import {***REMOVED***} from "@wordpress/block-editor";
 
-const typeOptions = [{label: "Data", value: "data"},
-    {label: "Base", value: "base"}]
+const typeOptions = [{label: "Data", value: "data"}, {label: "Base", value: "base"}]
 
 const toOptions = (files) => {
     return [{label: 'None', value: 'none'}, ...files.map(file => {
@@ -16,39 +26,51 @@ const toOptions = (files) => {
 }
 
 const Layer = (props) => {
-    const {onChange, metadata, layer, layer: {name, type, file, app}} = props
+    const {onChange, metadata, layer, layer: {name, shapeColor, labelFilter, type, file, app, labelField}} = props
 
     const [files, setFiles] = useState([])
+    const [features, setFeatures] = useState([])
     useEffect(() => {
+        debugger;
         getJsonFiles().then(files => {
             setFiles(toOptions(files))
+            debugger;
         })
     }, [])
 
+
+    useEffect(() => {
+        debugger;
+        fetch(file).then(response => response.json()).then(data => {
+            setFeatures(data.features);
+            console.log(data.features)
+        });
+    }, [layer.file])
     const ***REMOVED*** = (atrr, value) => {
+        debugger
         console.log("change attribute " + atrr + " to " + value)
         const newLayer = {...layer}
         newLayer[atrr] = value
         onChange(newLayer)
     }
 
-    return [
-        <PanelRow>
-            <TextControl
-                type={"String"}
-                label={__("Name", "dg")}
-                onChange={name => ***REMOVED***("name", name)}
-                value={name}
-            />
-        </PanelRow>,
-        <PanelRow>
-            <SelectControl
-                label={__("Type", "dg")}
-                value={type}
-                onChange={type => ***REMOVED***("type", type)}
-                options={typeOptions}
-            />
-        </PanelRow>,
+    debugger;
+    return [<PanelRow>
+        <TextControl
+            type={"String"}
+            label={__("Name", "dg")}
+            onChange={name => ***REMOVED***("name", name)}
+            value={name}
+        />
+    </PanelRow>, <PanelRow>
+        <SelectControl
+            label={__("Type", "dg")}
+            value={type}
+            onChange={type => ***REMOVED***("type", type)}
+            options={typeOptions}
+        />
+    </PanelRow>, ,
+
         <PanelRow>
             <SelectControl
                 type={"String"}
@@ -59,8 +81,77 @@ const Layer = (props) => {
 
             </SelectControl>
         </PanelRow>,
+        <PanelBody title={"Colors"} initialOpen={false}>
+            <***REMOVED***
+                title={__(`Fill Color`)}
+                colorSettings={[{
+                    value: layer.fillColor, onChange: (fillColor) => {
+                        ***REMOVED***("fillColor", fillColor)
+                    },
+
+                }]}
+            />
+            <***REMOVED***
+                title={__(`Border Color`)}
+                colorSettings={[{
+                    value: layer.borderColor, onChange: (borderColor) => {
+                        ***REMOVED***("borderColor", borderColor)
+                    },
+
+                }]}
+            />
+
+            <***REMOVED***
+                title={__(`Label Color`)}
+                label="Color"
+                colorSettings={[{
+
+                    value: layer.labelColor, onChange: (labelColor) => {
+                        ***REMOVED***("labelColor", labelColor)
+                    },
+
+                }]}
+            />
+
+        </PanelBody>,
+
+        <PanelBody title={"Labels"} initialOpen={false}>
+
+            <Property
+                title={"Label Field"}
+                property={"labelField"} value={labelField}
+                ***REMOVED***={***REMOVED***}
+                features={features}/>
+
+
+            <PanelRow>
+                <RangeControl
+                    label="Size"
+                    value={layer.labelFontSize * 1000}
+                    onChange={(labelFontSize) => ***REMOVED***("labelFontSize", labelFontSize / 1000)}
+                    min={0}
+                    step={1}
+                    max={500}
+                />
+            </PanelRow>
+
+            {labelField != 'none' && <PanelRow>
+                <PanelBody title={"Show/Hidde Labels"}>
+                    {features && features.map(feature => <ToggleControl
+                        label={feature.properties[labelField]}
+                        checked={labelFilter.indexOf(feature.properties[labelField]) == -1}
+                        onChange={(selected) => {
+                            debugger;
+                            ***REMOVED***("labelFilter", !selected ? [...layer.labelFilter, feature.properties[labelField]] : layer.labelFilter.filter(f => f !== feature.properties[labelField]))
+                        }}
+                    />)}
+
+                </PanelBody>
+            </PanelRow>}
+        </PanelBody>,
+
         <React.Fragment>
-            {type=='data'&&<PanelBody initialOpen={false} title={__("API & Source")}>
+            {type == 'data' && <PanelBody initialOpen={false} title={__("API & Source")}>
                 <PanelRow>
                     <SelectControl
                         value={[app]} // e.g: value = [ 'a', 'c' ]
@@ -70,6 +161,8 @@ const Layer = (props) => {
                         options={metadata.apps}
                     />
                 </PanelRow>
+
+
                 <***REMOVED***
                     ***REMOVED***={***REMOVED***}
                     allDimensions={metadata.dimensions}
@@ -77,8 +170,10 @@ const Layer = (props) => {
                     allMeasures={metadata.measures}
                     allCategories={metadata.categories}
                     allApps={metadata.apps}
+                    features={features}
                     layer={layer}>
                 </***REMOVED***>
+
             </PanelBody>}
         </React.Fragment>,
 
@@ -106,9 +201,7 @@ class ***REMOVED*** extends BlockEditWithAPIMetadata {
                 const apps = data.applications ? [...data.applications.application
                     .filter(a => a.instance[0].metadata.type === 'data')
                     .map(a => ({
-                        label: a.name,
-                        value: a.instance[0].vipAddress,
-                        settings: a.instance[0]
+                        label: a.name, value: a.instance[0].vipAddress, settings: a.instance[0]
                     })), {label: 'CSV', value: 'csv'}] : [{label: 'CSV', value: 'csv'}]
 
                 this.setState({...this.state, apps})
---
+++ wp-react-blocks-plugin/blocks/d3Map/Zoom.jsx
@@ -0,0 +1,9 @@
+import {PanelRow, TextControl} from "@wordpress/components";
+
+const ZoomControl = (props) => {
+
+    return [
+        <PanelRow>
+
+        </PanelRow>]
+}
