ae2dd0b0e94c72722cec1eaa62df166070c58bde
Sebastian Dimunzio <sdimunzio@gmail.com>
2023-06-20 10:11:34 -0300

    breaks administrator

--- wp-react-blocks-plugin/blocks/d3Map/BreaksGenerator.jsx
+++ wp-react-blocks-plugin/blocks/d3Map/BreaksGenerator.jsx
@@ -1,20 +1,89 @@
-import {Button, PanelRow, TextControl} from "@wordpress/components";
-import * as ss from "simple-statistics"
+import {Button, PanelBody, PanelRow, RangeControl, TextControl} from "@wordpress/components";
+import {***REMOVED***} from "@wordpress/block-editor";
+import {__} from '@wordpress/i18n';
+const ***REMOVED*** = ({***REMOVED***, breaks=[]}) => {
 
-const ***REMOVED*** = (props) => {
+    const add = () => {
+        const newBreaks = [...breaks]
+        newBreaks.push({
+            start: 0,
+            end: 1,
+            color: '#000000',
+            size: .1
+        })
+        ***REMOVED***("breaks", newBreaks)
+    }
 
-    
-    console.log(ss.jenks([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
+    const update = (property, value, index) => {
 
-    return <>
+    }
+
+    const remove=(index)=>{
+
+    }
+
+    return <PanelBody title={"Breaks"}>
+
+        {breaks.map((br, index) => {
+            return (<PanelBody title={"Breaks"}>
+                <PanelRow>
+                    <TextControl
+                        type={"Number"}
+                        label={__("Start", "dg")}
+                        value={br.start}
+                    />
+                </PanelRow>
+                <PanelRow>
+                    <TextControl
+                        type={"Number"}
+                        label={__("End", "dg")}
+                        value={br.end}
+                    />
+                </PanelRow>
+                <PanelRow>
+                    <RangeControl
+                        label="Maker Base Size"
+                        value={br.size}
+                        onChange={(value) => {
+                            update("size", index, value)
+                        }}
+                        step={0.5}
+                        min={0}
+                        max={10}
+                    />
+                </PanelRow>
+                <PanelRow>
+                    <***REMOVED***
+                        title={__(`Fill Color`)}
+                        colorSettings={[{
+                            value: br.color,
+                            onChange: (fillColor) => {
+
+                            },
+
+                        }]}
+                    />
+                    <***REMOVED***
+                        title={__(`Border Color`)}
+                        colorSettings={[{
+                            value: br.color,
+                            onChange: (borderColor) => {
+
+                            },
+
+                        }]}
+                    />
+                </PanelRow>
+            </PanelBody>)
+        })
+        }
+
+
         <PanelRow>
-            <Button isLink></Button>
+            <Button isLink onClick={e=>add()}>Add</Button>
+            <Button isLink onClick={e=>remove()}>Remove</Button>
         </PanelRow>
-        <PanelRow>
-            <Button isLink></Button>
-            <Button isLink></Button>
-        </PanelRow>
-    </>
+    </PanelBody>
 }
 
 
