ac26688eec0fd7e6bed4e8152d880e48236958ca
Sebastian Dimunzio <sdimunzio@gmail.com>
2023-06-22 09:37:26 -0300

    fixing zoom and adding point labels

--- wp-react-blocks-plugin/blocks/d3Map/BlockSave.js
+++ wp-react-blocks-plugin/blocks/d3Map/BlockSave.js
@@ -6,6 +6,7 @@ const SaveComponent = (props) => {
         attributes: {
             layers,
             height,
+            width,
             group,
             ***REMOVED***,
             mapPosition
@@ -21,6 +22,7 @@ const SaveComponent = (props) => {
         <div {...blockProps}
              className={"viz-component"}
              data-height={height}
+             data-width={width}
              data-group={group}
              data-back-ground-color={***REMOVED***}
              data-map-position={***REMOVED***(JSON.stringify(mapPosition))}
