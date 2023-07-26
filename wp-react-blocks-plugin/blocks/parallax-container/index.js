89a22e5d2b68d50735c152aaca6311e6530ce07a
Sebastian Dimunzio <sdimunzio@gmail.com>
2023-07-26 17:58:25 -0300

    adding parallax component

---
+++ wp-react-blocks-plugin/blocks/parallax-container/index.js
@@ -0,0 +1,59 @@
+import {__} from '@wordpress/i18n';
+import {***REMOVED***} from '@wordpress/blocks';
+import BlockSave from "./BlockSave";
+import BlockEdit from "./BlockEdit";
+import {Generic} from "../icons";
+
+
+***REMOVED***(process.env.BLOCKS_NS + '/parallax-container',
+    {
+        title: __('Parallaxed Post List', "dg"),
+        icon: Generic,
+        category: process.env.BLOCKS_CATEGORY,
+        attributes: {
+            panelStatus: {
+                type: "Object",
+                default: {}
+            },
+            count: {
+                type: 'numeric',
+                default: 7,
+            },
+            scrolls: {
+                type: 'numeric',
+                default: 7,
+            },
+            position: {
+                type: 'string',
+                default: "middle",
+            },
+
+            type: {
+                type: 'string',
+                default: "posts",
+            },
+            taxonomy: {
+                type: 'string',
+                default: "none",
+            },
+            categories: {
+                type: 'array',
+                default: [],
+            },
+            height: {
+                type: 'Numeric',
+                default: 500,
+            },
+
+            configuration: {
+                type: 'array',
+                default: [],
+            }
+
+
+        },
+        edit: BlockEdit,
+        save: BlockSave
+    }
+)
+;
