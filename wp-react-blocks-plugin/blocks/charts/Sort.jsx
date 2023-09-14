f506064b778c7d9b9e957060397f5e2c822e9280
Sebastian Dimunzio <sdimunzio@gmail.com>
2023-09-14 18:22:13 -0300

    updating chart components

---
+++ wp-react-blocks-plugin/blocks/charts/Sort.jsx
@@ -0,0 +1,47 @@
+import {PanelRow, RangeControl, TextControl, SelectControl, ToggleControl} from '@wordpress/components';
+import {__} from '@wordpress/i18n';
+
+const Sort = (props) => {
+    const {
+        toggleSelection,
+        setAttributes,
+        attributes: {app, sort, sortReverse}
+    } = props;
+
+    if (app != 'CSV') {
+        return [<PanelRow>
+            <SelectControl
+                label={__('Sort Data: ', "dg")}
+                value={[sort]}
+                onChange={(sort) => {
+                    setAttributes({sort})
+
+                }}
+                options={[{label: __('Default', "dg"), value: 'default'}, {
+                    label: __('Alphabetically ', "dg"),
+                    value: 'alphabetically'
+
+                }, {label: __('Value ', "dg"), value: 'values'}]}
+            />
+        </PanelRow>,
+            <PanelRow>
+                <ToggleControl
+                    label={__('Reverse Sort ', "dg")}
+                    value={[sortReverse]}
+                    onChange={(sortReverse) => {
+                        setAttributes({sortReverse})
+
+                    }}
+                    checked={sortReverse}
+
+                />
+            </PanelRow>
+
+        ]
+    }
+    return []
+}
+
+export default Sort
+
+
