8c0def31e693a14a53c6e8ff41d7999238cf7dbc
mbertoli <mfbertoli@gmail.com>
2023-11-27 11:19:04 -0300

    -add filters to sankey chart

--- wp-react-blocks-plugin/blocks/commons/APIConfig.js
+++ wp-react-blocks-plugin/blocks/commons/APIConfig.js
@@ -2,6 +2,7 @@ import {Component} from "@wordpress/element";
 import {__} from '@wordpress/i18n';
 import {Button, PanelBody, PanelRow, SelectControl, ToggleControl} from '@wordpress/components';
 import ChartMeasures from './ChartMeasures.jsx'
+import DataFilters from "./DataFilters";
 
 const defaultFormat = {
     "style": "percent",
@@ -10,37 +11,6 @@ const defaultFormat = {
     "currency": "USD"
 }
 
-
-const FilterSelector = ({param, index, options, onUpdateFilterParam}) => {
-    const sortedOptions = options.sort(function (a, b) {
-        var aLabel = a.label ? a.label.toLowerCase() : "";
-        var bLabel = b.label ? b.label.toLowerCase() : "";
-        return aLabel < bLabel ? -1 : aLabel > bLabel ? 1 : 0;
-    });
-
-    return <SelectControl onChange={(value) => {
-        onUpdateFilterParam(value, index)
-    }} value={param} options={sortedOptions}/>
-}
-
-const CategoricalFilter = ({value, index, items, onUpdateFilterValue}) => {
-    if (items) {
-        const sortedItems = items.sort(function (a, b) {
-            /*
-                var aValue= a.value ? a.value.toLowerCase() : "";
-                var bValue = b.value ? b.value.toLowerCase() : "";
-                return aValue < bValue ? -1 : aValue > bValue ? 1 : 0;
-            */
-            return a.position - b.position
-        });
-        return sortedItems.map(v => <PanelRow> <ToggleControl label={v.value} checked={value.indexOf(v.id) > -1}
-                                                              onChange={e => {
-                                                                  onUpdateFilterValue(v.id, index)
-                                                              }}/></PanelRow>)
-    } else {
-        return null;
-    }
-}
 
 /*let types = [
     {label: 'Bar', value: 'bar', supports: {singleMeasure: false, singleDimension: false}},
@@ -54,12 +24,6 @@ export class APIConfig extends Component {
 
         this.onMeasuresChange = this.onMeasuresChange.bind(this)
         this.onSetSingleMeasure = this.onSetSingleMeasure.bind(this)
-        this.addFilter = this.addFilter.bind(this)
-        this.updateFilterParam = this.updateFilterParam.bind(this)
-        this.updateFilterValue = this.updateFilterValue.bind(this)
-        this.setFilterValue = this.setFilterValue.bind(this)
-        this.removeFilter = this.removeFilter.bind(this)
-        this.items = this.items.bind(this)
 
         this.onFormatChange = this.onFormatChange.bind(this)
         this.onCustomLabelToggleChange = this.onCustomLabelToggleChange.bind(this)
@@ -79,59 +43,6 @@ export class APIConfig extends Component {
 
         const {setAttributes} = this.props
         setAttributes({measures: [], filters: []})
-    }
-
-    updateFilterParam(param, idx) {
-        const {attributes: {filters}, setAttributes, allFilters} = this.props
-        const newFilters = filters.slice()
-        const selected = allFilters.filter(f => f.param === param)[0]
-        newFilters[idx] = {...selected, value: []}
-        setAttributes({filters: newFilters})
-
-    }
-
-    updateFilterValue(value, idx) {
-        const {attributes: {filters}, setAttributes, allFilters} = this.props
-        const selected = filters[idx]
-        let values = selected.value
-        if (values.indexOf(value) > -1) {
-            values = values.filter(v => v != value)
-        } else {
-            values.push(value)
-        }
-
-        const newFilters = filters.slice()
-        newFilters[idx].value = values
-        setAttributes({filters: newFilters})
-    }
-
-    setFilterValue(value, idx) {
-        const {attributes: {filters}, setAttributes, allFilters} = this.props
-        const selected = filters[idx]
-        let values = selected.value
-        values = value.split(",")
-        const newFilters = filters.slice()
-        newFilters[idx].value = values
-        setAttributes({filters: newFilters})
-
-    }
-
-    addFilter() {
-        const {attributes: {filters}, setAttributes, allFilters} = this.props
-        let index = filters.length > allFilters.length ? allFilters.length : filters.length
-        const newFilter = (allFilters && allFilters.length > 0) ? {
-            ...allFilters[index],
-            "value": []
-        } : null
-        let newFilters = filters.slice()
-        newFilters.push(newFilter)
-        setAttributes({filters: newFilters})
-    }
-
-    removeFilter(f) {
-        const {attributes: {filters}, setAttributes, allFilters} = this.props
-        let newFilters = filters.slice(0, -1)
-        setAttributes({filters: newFilters})
     }
 
 
@@ -335,7 +246,6 @@ export class APIConfig extends Component {
                 </PanelRow>}
             </PanelBody>,
                 <ChartMeasures
-
                     onFormatChange={this.onFormatChange}
                     onUseCustomAxisFormatChange={this.onUseCustomAxisFormatChange}
                     onSetSingleMeasure={this.onSetSingleMeasure}
@@ -345,28 +255,7 @@ export class APIConfig extends Component {
                     {...this.props}
                     currentType={currentType}/>,
 
-
-                <>
-                    <PanelBody initialOpen={false} title={__("Filters")}>
-                        {filters.map((f, index) => {
-
-                            return (
-                                <PanelBody initialOpen={true} title={__(`Filter - ${f.label}`)}>
-                                    <FilterSelector param={f.param} index={index} options={allFilters}
-                                                    onUpdateFilterParam={this.updateFilterParam}/>
-                                    {<CategoricalFilter value={f.value} index={index} items={this.items(f.type)}
-                                                        onUpdateFilterValue={this.updateFilterValue}/>}
-                                </PanelBody>)
-                        })}
-
-                        <PanelRow>
-
-                            <Button variant={"link"} onClick={this.addFilter}>{__("Add Filter")}</Button>
-                            <Button variant={"link"} onClick={this.removeFilter}>{__("Remove")}</Button>
-                        </PanelRow>
-                    </PanelBody>
-
-                </>
+                <DataFilters {...this.props}/>
             ]
         )
     }
