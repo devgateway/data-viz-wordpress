8424cc75d35535c5dd12c36a8f080104b7c41a8b
mbertoli <mfbertoli@gmail.com>
2023-12-26 17:43:17 -0300

    CNFA-987

---
+++ wordpress/wp-react-blocks-plugin/blocks/data-labels/BlockSave.js
@@ -0,0 +1,43 @@
+import {useBlockProps} from '@wordpress/block-editor';
+import {InnerBlocks} from '@wordpress/editor'; // or wp.editor
+const SaveComponent = (props) => {
+    const {attributes: {
+            measures,
+            height,
+            dimension1,
+            app,
+            format,
+            filters,
+            group,
+            valueType,
+            noDataMsg
+        }
+    } = props;
+    const blockProps = useBlockProps.save({
+        className: 'data label'
+    });
+
+    const levels = [dimension1]
+    const source = levels.filter(l => l != 'none' && l != null).join('/')
+
+    return (
+        <div {...blockProps} className={"viz-component"}
+             data-component={"datalabel"}
+             data-height={height}
+             data-source={source}
+             data-dimension1={dimension1}
+             data-app={app}
+             data-measures={***REMOVED***(JSON.stringify(measures))}
+             data-format={***REMOVED***(JSON.stringify(format))}
+             data-group={group}
+             data-filters={***REMOVED***(JSON.stringify(filters))}
+             data-no-data-message={noDataMsg}
+             data-value-type={valueType}
+>
+
+            <InnerBlocks.Content/>
+        </div>
+    );
+}
+
+export default SaveComponent
