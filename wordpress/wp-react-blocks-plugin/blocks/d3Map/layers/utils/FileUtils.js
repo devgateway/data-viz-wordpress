d6d79d03c789cbb89e288dffb16e7d2013ac32b7
mbertoli <mfbertoli@gmail.com>
2024-04-09 17:23:17 -0300

    CNFA-1351

--- wordpress/wp-react-blocks-plugin/blocks/d3Map/layers/utils/FileUtils.js
+++ wordpress/wp-react-blocks-plugin/blocks/d3Map/layers/utils/FileUtils.js
@@ -1,6 +1,5 @@
 export const getJsonFiles = () => {
-    return wp.apiFetch({path: '/wp/v2/media?mime_type=application/json'});
+    return wp.apiFetch({path: '/wp/v2/media?per_page=100&mime_type=application/json'});
 }
-
 
 export default this
