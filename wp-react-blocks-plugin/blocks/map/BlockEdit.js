a280aa66b1c0cd8c9e1d9f3e6878d42ba884f7e0
mbertoli <mfbertoli@gmail.com>
2024-04-09 17:23:17 -0300

    CNFA-1351

--- wp-react-blocks-plugin/blocks/map/BlockEdit.js
+++ wp-react-blocks-plugin/blocks/map/BlockEdit.js
@@ -119,7 +119,7 @@ class BlockEdit extends BlockEditWithAPIMetadata {
         const ***REMOVED*** = taxonomy != 'none' && fileType!= 'none' ? `&${taxonomy}=${fileType}` :''
         const mapFiles = [{value:'',  label: 'None'}]
         wp.apiFetch({
-            path: '/wp/v2/media?mime_type=application/json' + ***REMOVED***,
+            path: '/wp/v2/media?per_page=100&mime_type=application/json' + ***REMOVED***,
         }).then(json => {
             if (json) {
                 json.forEach(f => {
