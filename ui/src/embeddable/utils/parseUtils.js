cac45bbf790d373c0aa5b4a9b5f3cbb087e06db7
mbertoli <mfbertoli@gmail.com>
2024-04-17 08:44:53 -0300

    CNFA-1358

--- ui/src/embeddable/utils/parseUtils.js
+++ ui/src/embeddable/utils/parseUtils.js
@@ -2,7 +2,7 @@ export const decode = (value, editing) => {
     if (editing) {
         return value
     }
-    return ***REMOVED***(value)
+    return ***REMOVED***(value.replaceAll("%", "%25")) //replaces "%" character before decode URI to avoid errors with URI codes
 }
 
 export const parse = (value, editing) => {
