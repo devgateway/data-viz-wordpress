5da11dbbda1a835be52bb868b947446b5b43ddc1
Hush <allan.mogusu@ledr.io>
2024-05-24 11:33:23 +0300

    update: ui layout changes

---
+++ ui/src/layout/FlexWrapDetector.jsx
@@ -0,0 +1,66 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+
+const ***REMOVED*** = ({ children, onWrapChange, className }) => {
+  const containerRef = useRef(null);
+  const [wrapCount, setWrapCount] = useState(0);
+
+  const makeFlexWrap = useCallback(() => {
+    if (containerRef.current) {
+      containerRef.current.style.setProperty("display", "flex", "important");
+      if (wrapCount > 0) {
+        containerRef.current.style.setProperty(
+          "flex-wrap",
+          "wrap",
+          "important"
+        );
+      } else {
+        containerRef.current.style.setProperty(
+          "flex-wrap",
+          "nowrap",
+          "important"
+        );
+      }
+    }
+  }, [wrapCount]);
+
+  const checkWrap = useCallback(() => {
+    const container = containerRef.current;
+    let count = 0;
+    if (container && container.children.length > 1) {
+      const firstTop = container.children[0].getBoundingClientRect().top;
+      Array.from(container.children).forEach((child, index) => {
+        if (index > 0 && child.getBoundingClientRect().top > firstTop) {
+          count++;
+        }
+      });
+    }
+
+    if (count !== wrapCount) {
+      setWrapCount(count);
+    }
+  }, [wrapCount]);
+
+  useEffect(() => {
+    checkWrap();
+    window.***REMOVED***("resize", checkWrap);
+    return () => window.***REMOVED***("resize", checkWrap);
+  }, [checkWrap]);
+
+  useEffect(() => {
+    if (onWrapChange) {
+      onWrapChange(wrapCount);
+    }
+  }, [wrapCount, onWrapChange]);
+
+  useEffect(() => {
+    makeFlexWrap()
+  }, [makeFlexWrap]);
+
+  return (
+    <div ref={containerRef} className={className}>
+      {makeFlexWrap()}
+      {children}
+    </div>
+  );
+};
+export default ***REMOVED***;
--- ui/src/layout/SearchControl.jsx
+++ ui/src/layout/SearchControl.jsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from "react";
-import CustomSearch from './***REMOVED***'
 import ***REMOVED*** from "@devgateway/wp-react-lib/dist/providers/***REMOVED***";
 import ***REMOVED*** from "@devgateway/wp-react-lib/dist/consumers/***REMOVED***";
 import {injectIntl} from "react-intl";
@@ -8,17 +7,11 @@ import ***REMOVED*** from "./***REMOVED***";
 
 
 const ***REMOVED*** = injectIntl(({
-                                       ID,
                                        title,
                                        slug,
-                                       parent_title,
-                                       parent_slug,
                                        parent_link,
                                        extract,
-                                       type,
                                        link,
-                                       terms,
-                                       subtype,
                                        bread_crumbs = [],
                                        metadata: {redirect_url},
                                        intl: {locale}
@@ -28,10 +21,8 @@ const ***REMOVED*** = injectIntl(({
     let target = parent_link ? utils.replaceLink(parent_link, locale) + `#${slug}` : utils.replaceLink(link, locale)
     target = redirect_url ? redirect_url + `#${slug}` : target
 
-    
-
     return (
-        <div className={"has-standard-12-font-size"} onClick={e => document.location.href = target}>
+        <div className={"has-standard-12-font-size"} onClick={() => document.location.href = target}>
             <h5>{bread_crumbs && bread_crumbs.length > 0 ? `${bread_crumbs.join(' / ')}` : ''} </h5>
             <div className={"has-standard-14-font-size"}><h4>{title}</h4></div>
             <div className='search-content' dangerouslySetInnerHTML={{__html: utils.***REMOVED***(extract, locale)}}/>
@@ -41,16 +32,16 @@ const ***REMOVED*** = injectIntl(({
 })
 
 const replaceString = (content, words) => {
-    let regex = RegExp(words, 'gi')  
+    let regex = RegExp(words, 'gi')
     let newHTML = content
     let instances = [...(newHTML.matchAll(regex))]
     let shift = 0
     let ***REMOVED*** = newHTML.length
-    instances.forEach(instance => {                             
+    instances.forEach(instance => {
         let replacement = '<b>' + newHTML.substring(instance.index + shift, instance.index + shift + words.length) + '</b>';
         newHTML = newHTML.substring(0, instance.index + shift) + replacement + newHTML.substring(instance.index + words.length + shift);
-        shift = newHTML.length - ***REMOVED***;              
-    }) 
+        shift = newHTML.length - ***REMOVED***;
+    })
 
     return newHTML;
 }
@@ -60,15 +51,15 @@ const ***REMOVED*** = (words) => {
     let ***REMOVED*** = searchedPara = searchedPara = document.***REMOVED***('H5')
     let searchResult = searchedPara = searchedPara = document.***REMOVED***('.search-content')
     for (let i = 0; i < searchResult.length; i++) {
-        if (searchResult[i]) {          
+        if (searchResult[i]) {
             searchResult[i].innerHTML = replaceString(searchResult[i].textContent, words);
-        }      
+        }
     }
-    
+
     for (let i = 0; i < ***REMOVED***.length; i++) {
        if (***REMOVED***[i]) {
             ***REMOVED***[i].innerHTML = replaceString(***REMOVED***[i].textContent, words);
-      }        
+      }
     }
 }
 
@@ -95,7 +86,7 @@ const SearchControl = ({onSearch,perPage, loading, results, meta, locale}) => {
             value={searchTerm}
             loading={loading}
             placeholder={placeholder}
-            ***REMOVED***={(e, data) => null}
+            ***REMOVED***={() => null}
             total={total}
             perPage={perPage}
             totalPages={totalPages}
