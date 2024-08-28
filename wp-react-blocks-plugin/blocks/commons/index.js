8578943e5fcf77f6f6e15e2fe1d59bce56c310c3
allan_dg <amogusu@developmentgateway.org>
2024-08-28 15:49:25 +0300

    chore: handle edge cases around loading of measure and category data

--- wp-react-blocks-plugin/blocks/commons/index.js
+++ wp-react-blocks-plugin/blocks/commons/index.js
@@ -372,7 +372,7 @@ export class BlockEditWithAPIMetadata extends ComponentWithSettings {
                     return response.json()
                 })
                 .then(data => {
-                    sessionStorage.setItem('measures', JSON.stringify(getTranslatedOptions(data)))
+                    sessionStorage.setItem(`measures_${app}`, JSON.stringify(getTranslatedOptions(data)))
                     this.setState({...this.state, measures: getTranslatedOptions(data)})
                 })
                 .catch(function (response) {
@@ -387,7 +387,7 @@ export class BlockEditWithAPIMetadata extends ComponentWithSettings {
                     return response.json()
                 })
                 .then(data => {
-                        sessionStorage.setItem('categories', JSON.stringify(data))
+                        sessionStorage.setItem(`categories_${app}`, JSON.stringify(data))
                         this.setState({...this.state, categories: getTranslatedOptions(data)})
                     }
                 )
