8e23c2fdbfa18d22591e87d1cbaf937da455b798
Timothy Mugo <tmugo@developmentgateway.org>
2024-10-03 14:41:08 +0300

    DVIZ-10 add wrapped and datalabel component

---
+++ new-ui/src/embeddable/wrapped/index.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import * as external from "@devgateway/customizer";
+
+const Root = (props) => {
+    const [random, ***REMOVED***] = useState(Math.random() * (99999 - 1) + 1);
+    const {
+        "data-width": width,
+        "data-height": height,
+        "data-name": name, parent, editing, component, unique
+    } = props
+
+    let C2: React.FC = () => {
+        return <div>Not found</div>
+    }
+    if (external[name]) {
+        C2 = external[name]
+    } else {
+        C2 = React.lazy(() => import('../' + name + '/'))
+    }
+    return (<div style={{width: '100%', height: height + "px"}} className={"parallax-container"}>
+
+        <React.Suspense fallback={<div>Loading...</div>}>
+            <C2 {...props} />
+        </React.Suspense>
+    </div>)
+
+}
+
+
+export default Root
