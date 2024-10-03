b18610a482b54645b78a6406177f6693112f1333
Timothy Mugo <tmugo@developmentgateway.org>
2024-10-03 15:18:46 +0300

    DVIZ-10 add d3 map

---
+++ new-ui/src/embeddable/d3Map/index.jsx
@@ -0,0 +1,116 @@
+import React, {useEffect, ***REMOVED***, useRef, useState} from 'react';
+import {connect} from "react-redux";
+import {decode, parse, ***REMOVED***} from "../utils/parseUtils";
+import Map from "./Map"
+import BaseLayer from './BaseLayer'
+import DataLayer from './DataLayer'
+import LatLongLayer from './LatLongLayer'
+import ZoomControl from "./ZoomControl";
+import {Container} from "semantic-ui-react";
+import ***REMOVED*** from "./***REMOVED***";
+import Legends from "./Legends"
+import FlowLayer from "./FlowLayer";
+
+
+const MapWrapper = (props) => {
+    const {
+        unique,
+        editing,
+        "data-group": group,
+        "data-layers": dataLayers,
+        "data-height": height = 400,
+        "data-width": width = 1000,
+        "data-back-ground-color": bgColorParam = '#88e8dc',
+        "data-map-position": ***REMOVED*** = {},
+        "data-projection": ***REMOVED*** = "geoMercator",
+        "data-zoom-enabled": zoomEnabled = true,
+        "data-rotation-enabled": ***REMOVED*** = false,
+        intl
+    } = props
+
+
+    const [layers, setLayers] = useState(parse(dataLayers))
+    const ref = useRef(null);
+    const zoomRef = useRef(null);
+    const [transform, setTransform] = useState(null)
+
+    useEffect(() => {
+        const newLayers = parse(dataLayers)
+        if (!***REMOVED***(layers, newLayers)) {
+            setLayers(newLayers)
+        }
+    }, [dataLayers])
+
+    const ***REMOVED*** = (id) => {
+        const newLayers = layers.slice()
+        const ly = newLayers.find(l => l.id == id);
+        if (ly) {
+            ly.visible = !ly.visible
+        }
+        setLayers(newLayers)
+    }
+
+
+    return (
+        <div ref={ref} className={"d3map-container"}>
+                <***REMOVED*** ***REMOVED***={decode(bgColorParam)}
+                                    height={height}
+                                    width={width}
+                                    ***REMOVED***={***REMOVED***}
+                                    editing={editing} ***REMOVED***={parse(***REMOVED***, editing)}>
+                    <Map ***REMOVED***={parse(***REMOVED***, editing)}>
+                        {layers.filter(l => l.visible != false).map((layer, i) => {
+                            if (layer.type === 'base') {
+                                return <BaseLayer transform={transform} intl={intl} zoom={zoomRef} unique={unique}
+                                                  key={i} {...layer} />
+                            }
+                            if (layer.type === 'data') {
+                                return <DataLayer  ***REMOVED***={e => {
+
+                                }
+                                } transform={transform} intl={intl}
+                                                  group={group} zoom={zoomRef}
+                                                  unique={unique}
+                                                  key={i} {...layer} />
+
+                            }
+                            if (layer.type === 'flow') {
+                                return <FlowLayer transform={transform} intl={intl} group={group} zoom={zoomRef}
+                                                  unique={unique}
+                                                  key={i} {...layer} />
+                            }
+                            if (layer.type === 'dataPoints') {
+                                return <LatLongLayer transform={transform} intl={intl} group={group} zoom={zoomRef}
+                                                     unique={unique}
+                                                     key={i} {...layer} />
+                            }
+
+                        })}
+
+
+                    </Map>
+
+                    <Legends patternsData={null} layers={layers} group={group} onItemClick={***REMOVED***}></Legends>
+
+
+                    <ZoomControl ***REMOVED***={parse(***REMOVED***, editing)}
+                                 zoomEnabled={parse(zoomEnabled, editing)} onZoomed={setTransform} width={width}
+                                 height={height} ref={zoomRef} group={group}
+                                 editing={editing}/>
+
+
+                </***REMOVED***>
+
+        </div>
+    );
+
+}
+
+
+const ***REMOVED*** = (state, ownProps) => {
+    return {}
+}
+
+const ***REMOVED*** = {};
+
+export default connect(***REMOVED***, ***REMOVED***)(MapWrapper)
