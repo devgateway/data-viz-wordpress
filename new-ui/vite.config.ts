6568721db504f960f2d8ac7ba93387065c107108
Timothy Mugo <tmugo@developmentgateway.org>
2024-10-23 17:56:11 +0300

    DVIZ-10 migrate env variables to vite env variables

--- new-ui/vite.config.ts
+++ new-ui/vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig, searchForWorkspaceRoot, loadEnv } from 'vite'
+import { defineConfig, searchForWorkspaceRoot, loadEnv, Plugin } from 'vite'
 import react from '@vitejs/plugin-react-swc';
 // @ts-ignore
 import eslintPlugin from 'vite-plugin-eslint';
 import path from "path";
-// import ***REMOVED*** from 'vite-plugin-environment';
+import Environment from 'vite-plugin-env-compatible';
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -11,35 +12,14 @@ export default defineConfig(({ mode }) => {
     console.log('env', env);
 
     return {
-        define: {
-            'process.env': env,
-        },
         plugins: [
             react({}),
             eslintPlugin({
                 exclude: ['/virtual:/**', 'node_modules/**', "dist/**"],
             }),
-            // ***REMOVED***({
-            //     API_URL: 'http://localhost:8000',
-            //     NODE_ENV: process.env.NODE_ENV,
-            //     APP_ENV: 'development',
-            //     PUBLIC_URL: '/',
-            //     PROTOCOL: 'http',
-            //     DOMAIN: 'localhost',
-            //     REACT_APP_TITLE: 'Tobacco Control Data Initiative',
-            //     REACT_APP_WP_API: 'http://localhost/wp/wp-json',
-            //     REACT_APP_WP_STYLES: `${process.env.PROTOCOL}://${process.env.DOMAIN}/wp/wp-admin/load-styles.php?c=1&dir=ltr&load%5Bchunk_0%5D=dashicons,admin-bar,buttons,media-views,editor-buttons,wp-components,wp-block-editor,wp-nux,wp-editor,wp-block-library,wp-block-&load%5Bchunk_1%5D=library-theme,wp-edit-blocks,wp-edit-post,wp-format-library,wp-block-directory,common,forms,admin-menu,dashboard,list-tables,edi&load%5Bchunk_2%5D=t,revisions,media,themes,about,nav-menus,wp-pointer,widgets,site-icon,l10n,wp-auth-check&ver=5.5.6' id='wp-block-library-css`,
-            //     REACT_APP_GA_CODE: 'TEST',
-            //     REACT_APP_DEFAULT_LOCALE: 'en',
-            //     REACT_APP_WP_HOSTS: 'http://localhost',
-            //     REACT_APP_USE_HASH_LINKS: 'false',
-            //     REACT_APP_THEME: 'cash',
-            //     REACT_APP_WP_SEARCH_END_POINT: '/dg/v1/search',
-            //     REACT_APP_API_ROOT: 'http://localhost',
-            // }, {
-            //     loadEnvFiles: true,
-
-            // })
+            Environment({
+                prefix: 'VITE_',
+            })
         ],
         resolve: {
             alias: {
