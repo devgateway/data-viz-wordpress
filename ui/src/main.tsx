3fbccd03a6d50c24aa4afcaa81cd4000f536e155
Timothy Mugo <tmugo@developmentgateway.org>
2024-12-09 20:29:32 +0300

    chore:TCDICORE-146 fix deps issue

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -6,7 +6,7 @@ import App from './App'
 
 import './scss/common.scss'; //core common styles
 import './scss/themes/default/index.scss'; // default theme styles
-import '@devgateway/customizer/dist/index.css'; //custom styles
+import '@devgateway/customizer/dist/css/index.css'; //custom styles
 import 'semantic-ui-css/semantic.min.css'; //semantic ui styles
 
 createRoot(document.***REMOVED***('root')!).render(
