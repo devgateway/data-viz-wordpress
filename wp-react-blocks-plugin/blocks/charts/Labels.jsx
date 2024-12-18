dea5f26e4430587419e40b50ea4d7120162e4f6d
allan_dg <amogusu@developmentgateway.org>
2024-12-18 20:39:33 +0700

    feat: add point label feature to line chart
    - this feature allows the line chart to display point labels like the
      bar chart implementation, at the top of the point only. By default,
    the label will be at the top

--- wp-react-blocks-plugin/blocks/charts/Labels.jsx
+++ wp-react-blocks-plugin/blocks/charts/Labels.jsx
@@ -1,27 +1,57 @@
-import {PanelRow, RangeControl, TextControl, SelectControl} from '@wordpress/components';
-import {__} from '@wordpress/i18n';
+import {
+  PanelRow,
+  SelectControl,
+} from "@wordpress/components";
+import { __ } from "@wordpress/i18n";
 
 const Labels = (props) => {
-    const {toggleSelection, setAttributes, attributes: {leftLegend, offsetY, dataSourceLabel, dataSource, barLabelPosition, type}} = props;
+  const {
+    setAttributes,
+    attributes: {
+      barLabelPosition,
+      lineLabelPosition,
+      type,
+    },
+  } = props;
 
-    return [
+  return [
+    <>
+      {type == "bar" && (
+        <PanelRow>
+          <SelectControl
+            label={__("Bar Label Position", "dg")}
+            value={[barLabelPosition]}
+            onChange={(barLabelPosition) => {
+              setAttributes({ barLabelPosition: barLabelPosition });
+            }}
+            options={[
+              { label: __("Top", "dg"), value: "top" },
+              { label: __("Middle", "dg"), value: "middle" },
+              { label: __("None", "dg"), value: "none" },
+            ]}
+            style={{ width: "120px" }}
+          />
+        </PanelRow>
+      )}
 
-        <>
-         {type == "bar" &&
-           <PanelRow>
-           <SelectControl
-               label={__('Bar Label Position',"dg")}
-               value={[barLabelPosition]} 
-               onChange={(barLabelPosition) => {
-                   setAttributes({barLabelPosition: barLabelPosition})
-                   
-               }}
-               options={[{label: __('Top', "dg"), value: 'top'}, {label: __('Middle', "dg"), value: 'middle'}, {label: __('None', "dg"), value: 'none'}]}
-               style={{width: "120px"}}
-           />
-          </PanelRow>
-        }
-        </>]
-}
+      {type == "line" && (
+        <PanelRow>
+          <SelectControl
+            label={__("Line Label Position", "dg")}
+            value={[lineLabelPosition]}
+            onChange={(position) => {
+              setAttributes({ lineLabelPosition: position });
+            }}
+            options={[
+              { label: __("On Top", "dg"), value: "top" },
+              { label: __("None", "dg"), value: "none" },
+            ]}
+            style={{ width: "120px" }}
+          />
+        </PanelRow>
+      )}
+    </>,
+  ];
+};
 
-export default Labels
+export default Labels;
