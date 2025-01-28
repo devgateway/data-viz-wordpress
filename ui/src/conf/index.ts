28294a4dde75bfa17122eea5f2f40a659b764b1a
Timothy Mugo <tmugo@developmentgateway.org>
2025-01-28 12:26:11 +0300

    chore: TCDICORE-169 fix api loading issue

--- ui/src/conf/index.ts
+++ ui/src/conf/index.ts
@@ -1,5 +1,5 @@
 export const Config = {
     REACT_APP_WP_API: process.env.VITE_REACT_APP_WP_API || "/wp/wp-json",
-    REACT_APP_API_ROOT: process.env.VITE_REACT_APP_WP_HOSTS || '',
+    REACT_APP_API_ROOT: process.env.VITE_REACT_APP_API_ROOT,
     GA_CODE: process.env.VITE_REACT_APP_GA_CODE
 }
