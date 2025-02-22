0fbb2fe08cf281c5f89c19c86084e9e9ae808f4b
gmutuhu <geraldmutuhu@gmail.com>
2025-02-22 17:49:42 +0300

    ALDASH-112 fix map issues
    ALDASH-112 fix map issues

--- wp-react-blocks-plugin/blocks/filter/BlockEdit.js
+++ wp-react-blocks-plugin/blocks/filter/BlockEdit.js
@@ -126,7 +126,7 @@ class BlockEdit extends BlockEditWithAPIMetadata {
                             <SelectControl
                                 value={app}
                                 onChange={(app) => {
-                                    setAttributes({app: app, hiddenFilters: [], apacheSupersetUrl: this.state.apache_superset_url})
+                                    setAttributes({app: app, hiddenFilters: []})
                                 }}
                                 options={this.state.apps}
                             />
