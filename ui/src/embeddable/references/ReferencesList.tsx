ede829c93301cce7c03eb7ec8b35e1c857ad6e66
Timothy Mugo <tmugo@developmentgateway.org>
2025-03-07 17:00:37 +0300

    chore (TCDICORE-206): fix references repsonsiveness by adjusting the stretching of the columns

--- ui/src/embeddable/references/ReferencesList.tsx
+++ ui/src/embeddable/references/ReferencesList.tsx
@@ -1,20 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grid, ***REMOVED*** } from 'semantic-ui-react'
+import { Container, Grid, ***REMOVED***, GridColumn, GridRow } from 'semantic-ui-react';
 import { connect } from "react-redux";
+import { useWindowDimensionsAndDevice } from "@/lib/hooks/window-dimensions";
 
 
 const Reference = ({ content, link, index }) => {
-    return (
-        <Grid.Column>
-            <Container id={"ref_" + index} className={"reference"}>
-                <div className={"index"}>{index}</div>
-                <div className={"content"} >{content}</div>
-                <div className={"link"}>
-                    <a href={link} target="_blank">{link}</a>
-                </div>
-            </Container>
-        </Grid.Column>
-    )
+    return <GridColumn>
+        <GridRow id={"ref_" + index} className={"reference"}>
+            <div className={"index"}>{index}</div>
+            <div className={"content"} >{content}</div>
+            <div className={"link"}>
+                <a href={link} target="_blank">{link}</a>
+            </div>
+        </GridRow>
+    </GridColumn>
 }
 
 const decodeContent = (content) => {
@@ -35,9 +34,12 @@ const References = ({
     "data-flex-direction": flexDirection = "row"
 }) => {
 
+    const { width: deviceWidth } = useWindowDimensionsAndDevice();
 
-    const style = { flexDirection, height: "auto" }
-    const [elements, setElements] = useState<NodeListOf<Element> | any []>([])
+    const isMobile = deviceWidth < 1250;
+
+    const style: { flexDirection: string; height?: string } = { flexDirection }
+    const [elements, setElements] = useState<NodeListOf<Element> | []>([])
 
     if (flexDirection == "column" && cols > 1) {
         style.height = height + "px"
@@ -50,13 +52,11 @@ const References = ({
                 const el = window.parent.document.***REMOVED***("div.wp-reference")
                 setElements(el)
             }, 3000)
-        } catch (err) {
-            console.error("Error loading references", err);
-        }
+        } catch (err) { /* empty */ }
     })
 
-    const items: any [] = elements ? new Array(...elements) : []
-    const unique: Element [] = []
+    const items = elements ? new Array(...elements) : []
+    const unique: Element[] = []
     items.forEach(item => {
         const found = unique.find(it => it.getAttribute("data-index") == item.getAttribute("data-index"))
         if (!found) {
@@ -64,20 +64,29 @@ const References = ({
         }
     })
 
+    console.log("columns", cols)
+    console.log("height", height)
+
     return (<Container className={"references list"}>
         {editing &&
             <div className="edit-mode-message"><p>No preview available. The full list of references will be displayed in the live page.</p></div>
         }
-        <Grid fluid stretched columns={cols as ***REMOVED***} style={style}>
-            {unique.sort((a, b) => {
-                const indexA = a.getAttribute("data-index") !== null ? parseInt(a.getAttribute("data-index")!) : Number.POSITIVE_INFINITY
-                const indexB = b.getAttribute("data-index") !== null ? parseInt(b.getAttribute("data-index")!) : Number.POSITIVE_INFINITY
-                return indexA - indexB
-            }).map(i => {
-                return <Reference key={i.getAttribute("data-index")} index={i.getAttribute("data-index")}
-                    content={decodeContent(i.getAttribute("data-description"))}
-                    link={i.getAttribute("data-link")}></Reference>
-            })}
+        <Grid fluid stretched={isMobile ? true : false } columns={cols as ***REMOVED***} style={style}>
+            {/* <GridRow style={style}> */}
+                {unique.sort((a, b) => {
+                    const indexA = a.getAttribute("data-index") ?? ""; // Handle null case
+                    const indexB = b.getAttribute("data-index") ?? ""; // Handle null case
+                    const numA = indexA ? parseInt(indexA) : Number.POSITIVE_INFINITY;
+                    const numB = indexB ? parseInt(indexB) : Number.POSITIVE_INFINITY;
+                    return numA - numB;
+                }).map(i => {
+                    const index = i.getAttribute("data-index") ?? ""; // Handle null case
+                    return <Reference key={index} index={index}
+                        content={decodeContent(i.getAttribute("data-description"))}
+                        link={i.getAttribute("data-link")}></Reference>
+                })}
+            {/* </GridRow> */}
+
         </Grid>
 
     </Container>)
