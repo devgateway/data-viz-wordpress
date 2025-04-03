b1a9608c5487170b8b120a8449ec5945b75d0454
gmutuhu <geraldmutuhu@gmail.com>
2025-04-03 13:00:49 +0300

    ALDASH-198 sort pre-set filter items
    ALDASH-198 sort pre-set filter items

--- wp-react-blocks-plugin/blocks/commons/DataFilters.jsx
+++ wp-react-blocks-plugin/blocks/commons/DataFilters.jsx
@@ -72,7 +72,7 @@ const DataFilters = (props) => {
   const ***REMOVED*** = ({value, index, items, ***REMOVED***}) => {
     if (items) {
       const sortedItems = items.sort(function (a, b) {
-        if (a.position !== undefined && b.position != undefined) {        
+        if (a.position !== undefined && b.position !== undefined) {        
           return a.position - b.position;
         }
   
