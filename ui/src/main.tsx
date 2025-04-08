bacec6cd1777a6c55f428c9f9affa9a430d8764d
allan_dg <amogusu@developmentgateway.org>
2025-04-08 14:10:11 +0300

    fix(TCDIZM-321): Make default theme loading optional
    - makes loading of default theme styles optional by use of an
      environment variable.

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
+import 'semantic-ui-css/semantic.min.css'; // semantic ui styles
+import './scss/common.scss'; // core common styles
 
-import 'semantic-ui-css/semantic.min.css'; //semantic ui styles
-import './scss/common.scss'; //core common styles
-import './scss/themes/default/index.scss'; //default theme styles
-import '@devgateway/customizer/dist/css/index.css'; //custom styles
-
+const rawEnv = import.meta.env.VITE_REACT_APP_LOAD_DEFAULT_THEME;
+const shouldLoadDefaultTheme = !(rawEnv && rawEnv.toString().toLowerCase() === 'false');
+if (shouldLoadDefaultTheme) {
+  import('./scss/themes/default/index.scss'); // default theme styles
+}
+import '@devgateway/customizer/dist/css/index.css'; // customizer styles
 
 createRoot(document.***REMOVED***('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
-
-// serviceWorker.unregister();
