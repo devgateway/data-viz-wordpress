9c8c9ece4cf77f9eafc9832d3a8077b1504988ed
Timothy Mugo <tmugo@developmentgateway.org>
2025-04-17 18:41:19 +0300

    refactor(DVIZ-32): move the commons folder into a standalone library, migrate the commons library to typescript, initialize PNPM workspaces

---
+++ packages/commons/src/ChartLegends.tsx
@@ -0,0 +1,213 @@
+import React from 'react';
+import {
+    Button,
+    ButtonGroup,
+    PanelBody,
+    PanelRow,
+    RangeControl,
+    TextControl,
+    ToggleControl
+} from '@wordpress/components';
+import {__} from '@wordpress/i18n';
+import {***REMOVED***} from '@wordpress/block-editor';
+
+export type ***REMOVED*** = 'left' | 'top' | 'right' | 'bottom';
+
+export type LegendProps = {
+    ***REMOVED***: ***REMOVED***;
+    setAttributes: (attributes: {
+        showLegends: boolean;
+        ***REMOVED***: ***REMOVED***;
+        marginLeft: number;
+        marginRight: number;
+        marginBottom: number;
+        marginTop: number;
+        legendLabel: string;
+        ***REMOVED***: boolean;
+        useCheckBoxBackground: boolean;
+        ***REMOVED***: string | null;
+        reverseLegend: boolean;
+    }) => void;
+
+    attributes: {
+        showLegends: boolean;
+        ***REMOVED***: ***REMOVED***;
+        marginLeft: number;
+        marginRight: number;
+        marginBottom: number;
+        marginTop: number;
+        legendLabel: string;
+        ***REMOVED***: boolean;
+        useCheckBoxBackground: boolean;
+        ***REMOVED***: string;
+        reverseLegend: boolean;
+    }
+}
+
+
+const ChartLegends = (props: LegendProps) => {
+    const {
+        setAttributes, ***REMOVED***, attributes
+    } = props;
+
+    const {
+        showLegends,
+        marginLeft,
+        marginRight,
+        marginBottom,
+        marginTop,
+        legendLabel,
+        ***REMOVED***,
+        useCheckBoxBackground,
+        ***REMOVED***,
+        reverseLegend
+    } = attributes;
+
+    return [
+        <PanelBody initialOpen={false}   title={__("Margins")}>
+            <PanelRow>
+                <RangeControl
+                    label={__('Margin Bottom (Space between chart area and bottom border)')}
+                    value={marginBottom}
+                    onChange={(marginBottom) => {
+                        if (marginBottom) {
+                            setAttributes({...attributes, marginBottom})
+                        }
+                    }}
+                    min={0}
+                    max={500}
+                />
+            </PanelRow>
+
+            <PanelRow>
+                <RangeControl
+                    label={__('Margin Left (Space between chart area and left border)')}
+                    value={marginLeft}
+                    ***REMOVED***={0}
+                    onChange={(marginLeft) => {
+                        if (marginLeft) {
+                            setAttributes({...attributes, marginLeft})
+                        }
+                    }}
+                    step={1}
+                    min={0}
+                    max={500}/>
+            </PanelRow>
+            <PanelRow>
+                <RangeControl
+                    label={__('Margin Right')}
+                    value={marginRight}
+                    onChange={(marginRight) => {
+                        if (marginRight) {
+                            setAttributes({...attributes, marginRight})
+                        }
+                    }}
+                    min={0}
+                    max={500}
+                />
+            </PanelRow>
+            <PanelRow>
+                <RangeControl
+                    label={__('Margin Top')}
+                    value={marginTop}
+                    onChange={(marginTop) => {
+                        if (marginTop) {
+                            setAttributes({...attributes, marginTop})
+                        }
+                    }}
+                    min={0}
+                    max={500}
+                />
+            </PanelRow>
+        </PanelBody>,
+        <PanelBody initialOpen={false}   title={__("Legends")}>
+
+            <PanelRow>
+                <ToggleControl
+                    label={__("Show Legends")}
+                    checked={showLegends}
+                    onChange={() => setAttributes({...attributes, showLegends: !showLegends})}
+                />
+            </PanelRow>
+           
+            {showLegends &&
+                <PanelRow>
+                    <ToggleControl
+                        label={__("Reverse legend order")}
+                        checked={reverseLegend}
+                        onChange={() => setAttributes({...attributes, reverseLegend: !reverseLegend })}
+                    />
+                </PanelRow>
+            }
+            {showLegends && <PanelRow>
+                <ToggleControl label={__("Use Label Background")} checked={***REMOVED***}
+                               onChange={() => setAttributes({...attributes, ***REMOVED***: !***REMOVED***})}></ToggleControl>
+
+            </PanelRow>
+            }
+            {showLegends && <PanelRow>
+                <ToggleControl label={__("Use Checkbox Background")} checked={useCheckBoxBackground}
+                               onChange={() => setAttributes({...attributes, useCheckBoxBackground: !useCheckBoxBackground})}></ToggleControl>
+
+            </PanelRow>
+
+            }
+           
+             {showLegends && <PanelRow>
+                <ButtonGroup>
+                    <Button variant={***REMOVED*** == 'left' ? 'primary' : 'secondary'}
+                            onClick={() => setAttributes({...attributes, ***REMOVED***: "left"})}>
+                        {__("Left")}
+                    </Button>
+                    <Button variant={***REMOVED*** == 'top' ? 'primary' : 'secondary'}
+                            onClick={() => setAttributes({...attributes, ***REMOVED***: "top"})}>
+                        {__("Top")}
+                    </Button>
+                    <Button variant={***REMOVED*** == 'right' ? 'primary' : 'secondary'}
+                            onClick={() => setAttributes({...attributes, ***REMOVED***: "right"})}>
+                        {__("Right")}
+                    </Button>
+                    <Button variant={***REMOVED*** == 'bottom' ? 'primary' : 'secondary'}
+                            onClick={() => setAttributes({...attributes, ***REMOVED***: "bottom"})}>
+                        {__("Bottom")}
+                    </Button>
+                </ButtonGroup>
+
+            </PanelRow>}
+            {showLegends && <PanelRow>
+                <TextControl
+                    label={__('Legends Title')}
+                    value={legendLabel}
+                    onChange={(legendLabel) => setAttributes({...attributes, legendLabel})}
+                />
+            </PanelRow>
+            }
+            {showLegends && <PanelRow>
+                <***REMOVED***
+                    title={__('Legends text color')}
+                    colorSettings={[
+                        {
+                            value: ***REMOVED***(***REMOVED*** ? ***REMOVED*** : "#000000"),
+
+                            onChange: (color) => {
+
+                                if (color) {
+                                    setAttributes({...attributes, ***REMOVED***: ***REMOVED***(color)})
+                                } else {
+                                    setAttributes({...attributes, ***REMOVED***: null})
+                                }
+                            },
+
+                            label: __("")
+                        }
+                    ]}
+
+                /> </PanelRow>}
+
+            
+        </PanelBody>,
+
+    ]
+}
+
+export default ChartLegends
---
+++ packages/commons/src/ChartMeasures.tsx
@@ -0,0 +1,255 @@
+import React from 'react';
+import { __ } from '@wordpress/i18n';
+import { ***REMOVED***, PanelBody, PanelRow, SelectControl, ToggleControl, TextControl } from '@wordpress/components';
+
+import Format from './Format.jsx'
+import { togglePanel } from "./Util.js";
+import { ***REMOVED*** } from "./APIutils.js";
+import { Measure } from './types.js';
+
+const defaultFormat = {
+    "style": "percent",
+    "minimumFractionDigits": 1,
+    "maximumFractionDigits": 1,
+    "currency": "USD"
+
+}
+
+export type ***REMOVED*** = {
+    ***REMOVED***: (measure: string) => void;
+    ***REMOVED***: (format: string, field: string) => void;
+    onUseCustomAxisFormatChange: (value: boolean) => void;
+    ***REMOVED***: (measure: string) => void;
+    onCustomLabelToggleChange: (measure: string) => void;
+    ***REMOVED***: (measure: string, value: string) => void;
+    allMeasures: any[];
+    setAttributes: (attributes: any) => void;
+    title: string;
+    attributes: {
+        panelStatus: any;
+        measures: Measure[];
+        dimension1: string;
+        dimension2: string;
+        type: string;
+        app: string;
+    }
+}
+
+
+
+export const ChartMeasures = (props: ***REMOVED***) => {
+    const {
+        ***REMOVED***,
+        ***REMOVED***,
+        onUseCustomAxisFormatChange,
+        ***REMOVED***,
+        onCustomLabelToggleChange,
+        ***REMOVED***,
+        allMeasures,
+        setAttributes,
+        title,
+        attributes: {
+            panelStatus,
+            measures,
+            dimension1,
+            dimension2,
+            type,
+            app
+        }
+    } = props
+
+
+
+
+    const MToggle = ({ measure }) => {
+        const userMeasure = measures[app] ? measures[app][measure.value] : {}
+
+        return (<ToggleControl
+            label={***REMOVED***(measure)}
+            checked={userMeasure ? userMeasure.selected : false}
+            onChange={(value) => ***REMOVED***(measure.value)} />)
+    }
+
+    const MCheckbox = ({ measure }) => {
+        const userMeasure = measures[app] ? measures[app][measure.value] : {}
+        return <***REMOVED***
+            label={***REMOVED***(measure)}
+            checked={userMeasure ? userMeasure.selected : false}
+            onChange={(value) => ***REMOVED***(measure.value)} />
+    }
+
+
+    const ***REMOVED*** = ({ measure, single }) => {
+        return <PanelRow>
+            {single && <MCheckbox measure={measure}></MCheckbox>}
+            {!single && <MToggle measure={measure}></MToggle>}
+        </PanelRow>
+
+    }
+
+    const countSelected = (g) => {
+        if (measures[app]) {
+            const mG = allMeasures.filter(f => ***REMOVED***(f.group) === g)
+            let count = 0
+            Object.keys(measures[app]).filter(l => mG.map(m => m.value).indexOf(l) > -1).forEach(k => {
+                if (measures[app][k].selected) {
+                    count++
+                }
+            })
+            return count
+        }
+        return 0
+    }
+    const countTotal = (g) => {
+        if (g) {
+            return allMeasures.filter(f => ***REMOVED***(f.group) === g).length
+        }
+
+        return 0
+    }
+
+    const ***REMOVED*** = () => {
+        if (measures[app] && allMeasures) {
+            return Object.keys(measures[app]).filter(k => measures[app][k].selected).map(k => {
+                return allMeasures.filter(m => m.value === k)[0]
+            }).filter(m => m)
+        }
+        return []
+    }
+
+    const ***REMOVED*** = ***REMOVED***()
+    return <><PanelBody title={title ? title : __("Measures")} initialOpen={panelStatus["MEASURES"]}
+        onToggle={e => togglePanel("MEASURES", panelStatus, setAttributes)}>
+
+        {
+            /*
+             Multiple measures conditions
+
+             Bar:
+             no dimensions selected
+             one dimension is selected
+             -  not available when second dimension gets selected
+
+             Line:
+               - Always multi measure as measures represents line series, one dimension should always be selected
+
+             Pie:
+                  no dimensions selected
+                   -  not available when any dimension is selected
+             */
+
+            ((type == 'line') ||
+                (type == 'bar' && dimension2 == 'none') ||
+                (type == 'pie' && dimension1 == 'none' && dimension2 == 'none')) && allMeasures && [...new Set(allMeasures.map(p => ***REMOVED***(p.group)))].map(g => {
+                    return (<PanelBody initialOpen={panelStatus[g]}
+                        onToggle={e => togglePanel(g, panelStatus, setAttributes)}
+                        title={`${g} (${countSelected(g)} / ${countTotal(g)} ) `}>
+                        {allMeasures.filter(f => ***REMOVED***(f.group) === g)
+                            .map(m => <PanelRow>
+                                <***REMOVED*** single={false} measure={m}></***REMOVED***>
+                            </PanelRow>)}
+                    </PanelBody>
+
+
+                    )
+                }
+                )
+        }
+
+
+        {
+            /*Single measure conditions
+
+        Bar:
+               2 dimensions selected
+         Line:
+                never
+         Pie:
+              any dimensions selected
+
+        */
+            ((type == 'bar' && dimension2 != 'none') || (type == 'pie' && (dimension1 != 'none' || dimension2 != 'none'))) && allMeasures && [...new Set(allMeasures.map(p => ***REMOVED***(p.group)))].map(g => {
+                return (<PanelBody
+                    initialOpen={panelStatus[g]}
+                    onToggle={e => togglePanel(g, panelStatus, setAttributes)}
+                    title={`${g} (${countSelected(g)} / ${allMeasures.filter(f => f.group === g).length} ) `}>
+
+                    {allMeasures.filter(f => ***REMOVED***(f.group) === g)
+                        .map(m => <PanelRow>
+                            <***REMOVED*** single={true} measure={m}></***REMOVED***>
+                        </PanelRow>)}
+
+                </PanelBody>
+                )
+            })
+
+
+        }
+
+        {
+            (type == 'overlay') && allMeasures && <SelectControl
+                label="Measure"
+                value={***REMOVED*** && ***REMOVED***[0] ? ***REMOVED***[0].value : null}
+                options={[{ value: '', label: 'Select Measure' }, ...allMeasures]}
+                onChange={(measure) => ***REMOVED***(measure)}
+                __nextHasNoMarginBottom
+            />
+
+        }
+
+
+        {(type != 'overlay') && <PanelBody title={__("Format")} initialOpen={panelStatus["FORMAT"]}
+            onToggle={e => togglePanel("FORMAT", panelStatus, setAttributes)}>
+            <Format
+                format={measures[app] && measures[app].format ? measures[app].format : defaultFormat}
+                customFormat={measures[app] && measures[app].customFormat ? measures[app].customFormat : defaultFormat}
+                ***REMOVED***={measures[app] ? measures[app].***REMOVED*** : false}
+                ***REMOVED***={(format, field) => {
+                    ***REMOVED***(format, field);
+                }}
+                onUseCustomAxisFormatChange={value => {
+                    onUseCustomAxisFormatChange(value);
+                }} />
+        </PanelBody>}
+
+    </PanelBody>
+        {(type != 'overlay') && ***REMOVED*** && ***REMOVED***.length > 0 &&
+            <PanelBody title={__("Measure Label Customization")} initialOpen={panelStatus["MEASURES_LABEL_CUSTOMIZATION"]}
+                onToggle={e => togglePanel("MEASURES_LABEL_CUSTOMIZATION", panelStatus, setAttributes)}>
+
+                {***REMOVED*** && [...new Set(***REMOVED***.map(p => ***REMOVED***(p.group)))].map(g => {
+                    return (<PanelBody initialOpen={panelStatus[g + "_LABEL_CUSTOMIZATION"]}
+                        onToggle={e => togglePanel(g + "_LABEL_CUSTOMIZATION", panelStatus, setAttributes)}
+                        title={`${g}`}>
+                        {***REMOVED***.filter(f => ***REMOVED***(f.group) === g)
+                            .map(m => {
+                                const userMeasure = measures[app] ? measures[app][m.value] : {}
+                                return (<><PanelRow><ToggleControl
+                                    label={***REMOVED***(m)}
+                                    checked={userMeasure ? userMeasure.***REMOVED*** : false}
+                                    onChange={(value) => onCustomLabelToggleChange(m.value)} /> </PanelRow>
+                                    {userMeasure.***REMOVED*** &&
+                                        <PanelRow>
+                                            <TextControl label={__("Custom Label")} value={userMeasure ? userMeasure.customLabel : ""} onChange={(value) => ***REMOVED***(m.value, value)} />
+                                        </PanelRow>
+                                    }
+                                </>)
+
+                            })}
+                    </PanelBody>
+
+
+                    )
+                }
+                )
+                }
+
+
+
+            </PanelBody>
+        }
+    </>
+}
+
+
+export default ChartMeasures
