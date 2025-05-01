1328b94fb13716f7c0f072471e9f0ae30d303c9e
Timothy Mugo <tmugo@developmentgateway.org>
2025-05-01 19:29:59 +0300

    chore(DVIZ-32): migrate meta-boxes to typescript

--- plugins/wp-react-blocks-plugin/blocks/meta-boxes/index.tsx
+++ plugins/wp-react-blocks-plugin/blocks/meta-boxes/index.tsx
@@ -1,19 +1,23 @@
+import React from "react";
 import { ***REMOVED*** } from "@wordpress/blocks";
 import { TextControl } from "@wordpress/components";
 import { useEntityProp, ***REMOVED*** } from "@wordpress/core-data";
 import { useBlockProps } from "@wordpress/block-editor";
 import { useDispatch, useSelect } from "@wordpress/data";
 
+// @ts-ignore Types are not available
 ***REMOVED***("myguten/meta-block", {
   title: "Meta Block",
   edit: ({ setAttributes, attributes }) => {
     const blockProps = useBlockProps();
     const postType = useSelect(
+      // @ts-ignore Types are not available
       (select) => select("core/editor").***REMOVED***(),
       []
     );
 
     const properties = useSelect(
+      // @ts-ignore Types are not available
       (select) => select("core/editor").getEditedPostAttribute("meta"),
       []
     );
