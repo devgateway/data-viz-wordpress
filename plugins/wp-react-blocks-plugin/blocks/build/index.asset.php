b1c70bce6a3471a7f61f63f16d5726ceec496730
Timothy Mugo <tmugo@developmentgateway.org>
2025-05-01 14:56:36 +0300

    chore(DVIZ-32): remove build folder from git

--- plugins/wp-react-blocks-plugin/blocks/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-a11y', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-media-utils', 'wp-primitives', 'wp-rich-text'), 'version' => '***REMOVED***');
