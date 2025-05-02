ed7626e626ee2a886fe7abae08cd8ed2e337c3a4
Timothy Mugo <tmugo@developmentgateway.org>
2025-05-02 12:00:39 +0300

    chore(DVIZ-32): migrate parallax-container to typescript

--- plugins/wp-react-blocks-plugin/blocks/parallax-container/index.js
+++ plugins/wp-react-blocks-plugin/blocks/parallax-container/index.js
@@ -2,14 +2,14 @@ import {__} from '@wordpress/i18n';
 import {registerBlockType} from '@wordpress/blocks';
 import BlockSave from "./BlockSave";
 import BlockEdit from "./BlockEdit";
-import {Generic} from "../icons";
+import { GenericIcon, BLOCKS_CATEGORY, BLOCKS_NS} from '@dg-data-viz/wp-commons';
 
 
-registerBlockType(process.env.BLOCKS_NS + '/parallax-container',
+registerBlockType(BLOCKS_NS + '/parallax-container',
     {
         title: __('Parallaxed Post List', "dg"),
-        icon: Generic,
-        category: process.env.BLOCKS_CATEGORY,
+        icon: GenericIcon,
+        category: BLOCKS_CATEGORY,
         attributes: {
             panelStatus: {
                 type: "Object",
