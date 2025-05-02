ea05dfbdf3cd52affcb7cbd9aefd2cd65c240f70
Timothy Mugo <tmugo@developmentgateway.org>
2025-05-02 11:28:29 +0300

    chore(DVIZ-32): migrate page-modules to typescript

--- plugins/wp-react-blocks-plugin/blocks/page-modules/index.js
+++ plugins/wp-react-blocks-plugin/blocks/page-modules/index.js
@@ -1,128 +1,15 @@
 import {__} from '@wordpress/i18n';
 import {***REMOVED***} from '@wordpress/blocks';
-import {***REMOVED***, useBlockProps} from '@wordpress/block-editor';
-import {Generic} from '../icons/index.js'
-import {Panel, PanelBody, PanelRow, ResizableBox, TextControl} from '@wordpress/components';
-import {ComponentWithSettings} from "../commons";
+import { GenericIcon, BLOCKS_CATEGORY, BLOCKS_NS } from '@dg-data-viz/wp-commons';
+import Edit from './BlockEdit';
+import SaveComponent from './BlockSave';
 
 
-const SaveComponent = (props) => {
-  const { navLabel, topTopLabel, previewMode } = props.attributes;
-  return (
-    <div
-      className={"viz-component"}
-      data-component={"pageModules"}
-      data-nav-label={navLabel}
-      data-to-top-label={topTopLabel}
-      data-preview-mode={previewMode}
-    />
-  );
-};
-
-class Edit extends ComponentWithSettings {
-    constructor(props) {
-        super(props);
-    }
-
-    ***REMOVED***() {
-        super.***REMOVED***();
-    }
-
-    ***REMOVED***() {
-        if (this.unsubscribe) {
-            this.unsubscribe()
-        }
-    }
-
-    ***REMOVED***(prevProps, prevState, snapshot) {
-        const ***REMOVED*** = this.state?.previewMode ?? 'Desktop';
-        if (***REMOVED*** !== prevState.previewMode) {
-            this.props.setAttributes({previewMode: ***REMOVED***})
-        }
-    }
-
-    render() {
-        const {attributes: {width, height, navLabel, topTopLabel, previewMode}, ***REMOVED***, setAttributes} = this.props;
-        const urlParams = new ***REMOVED***(window.location.search);
-        const parent = urlParams.get('post');
-        const queryString = `editing=true&parent=${parent}&data-nav-label=${navLabel}&data-to-top-label=${topTopLabel}&data-preview-mode=${previewMode}`;
-        const divClass = ""
-        const divStyles = {height: `${height}px`, width: '100%'}
-
-        return (
-            <div>
-
-                <***REMOVED***>
-                    <Panel header={__("Configuration","dg")}>
-                        <PanelBody title={__("Labels","dg")}>
-                            <PanelRow>
-                                <TextControl
-                                    label={__('Navigator Label',"dg")}
-                                    value={navLabel}
-                                    onChange={(navLabel) => setAttributes({navLabel})}
-                                />
-
-                            </PanelRow>
-
-                            <PanelRow>
-                                <TextControl
-                                    label={__('To Top Label',"dg")}
-                                    value={topTopLabel}
-                                    onChange={(topTopLabel) => setAttributes({topTopLabel})}
-                                />
-
-                            </PanelRow>
-                        </PanelBody>
-                    </Panel>
-                </***REMOVED***>
-                <ResizableBox
-                    size={{height}}
-                    style={{"margin": "auto", width: "100%"}}
-                    minHeight="200"
-                    minWidth="500"
-                    enable={{
-                        top: false,
-                        right: false,
-                        bottom: true,
-                        left: false,
-                        topRight: false,
-                        bottomRight: false,
-                        bottomLeft: false,
-                        topLeft: false,
-                    }}
-                    onResizeStop={(event, direction, elt, delta) => {
-                        setAttributes({
-                            height: parseInt(height + delta.height, 10),
-
-                        });
-                        ***REMOVED***(true);
-                    }}
-                    onResizeStart={() => {
-                        ***REMOVED***(false);
-                    }}
-                >
-
-                    <div>
-                        {this.state.react_ui_url&&<iframe
-                            style={{...divStyles}} className={divClass}
-                            scrolling={"no"}
-                            src={this.state.react_ui_url + "/embeddable/pagemodules?" + queryString}/>}
-                    </div>
-
-
-                </ResizableBox>
-            </div>
-        );
-
-
-    }
-}
-
-***REMOVED***(`${process.env.BLOCKS_NS}/page-modules`,
+***REMOVED***(`${BLOCKS_NS}/page-modules`,
     {
         title: __('Page Modules',"dg"),
-        icon: Generic,
-        category: process.env.BLOCKS_CATEGORY,
+        icon: GenericIcon,
+        category: BLOCKS_CATEGORY,
         attributes: {
             count: {
                 type: 'Numeric',
