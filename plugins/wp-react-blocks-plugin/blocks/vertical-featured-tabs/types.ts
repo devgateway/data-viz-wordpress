0b96fa397cffe9b5d7b9ae5346c82285b05a39cc
Timothy Mugo <tmugo@developmentgateway.org>
2025-05-02 14:20:37 +0300

    chore(DVIZ-32): migrate vertical-featured-tabs to typescript

---
+++ plugins/wp-react-blocks-plugin/blocks/vertical-featured-tabs/types.ts
@@ -0,0 +1,22 @@
+import { Categories, ComponentWithSettingsProps } from "@dg-data-viz/wp-commons";
+
+export interface VerticalFeaturedTabsAttributes {
+    count: number;
+    type: string;
+    taxonomy: string;
+    categories: Categories;
+    height: number;
+    colors: string[];
+    readMoreLabel: string;
+    coverWidth: number;
+    previewMode: string;
+}
+
+export interface VerticalFeaturedTabsProps extends ComponentWithSettingsProps {
+    attributes: VerticalFeaturedTabsAttributes;
+    setAttributes: (attributes: Partial<VerticalFeaturedTabsAttributes>) => void;
+    className: string;
+    src: string;
+    toggleSelection: (value: boolean) => void;
+    isSelected: boolean;
+}
