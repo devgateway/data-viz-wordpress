a3fa2132b885bb032a3aa86d165cee39b1629486
Timothy Mugo <tmugo@developmentgateway.org>
2025-05-08 12:34:37 +0300

    chore(DVIZ-39): create typescript and javascript templates for wp-customizer

---
+++ packages/create-wp-customizer/templates/template-ts/blocks/example/BlockEdit.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {***REMOVED***, useBlockProps } from '@wordpress/block-editor';
+import { PanelBody, TextControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { ExampleBlockEditProps } from './types';
+
+const BlockEdit = (props: ExampleBlockEditProps) => {
+  return (
+    <div {...useBlockProps()}>
+        <***REMOVED***>
+            <PanelBody title={__('Example Block', 'wp-customizer-react-blocks')} initialOpen={true}>
+                <TextControl label={__('Text', 'wp-customizer-react-blocks')} value={props.attributes.text} onChange={props.setAttributes.bind(this)} />
+            </PanelBody>
+        </***REMOVED***>
+    </div>
+  )
+}
+
+export default BlockEdit;
