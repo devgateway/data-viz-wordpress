b0cc4e974d3f1d653b74444b69249b7d028a0c42
gmutuhu <geraldmutuhu@gmail.com>
2025-12-15 10:47:38 +0300

    ALDASH-511 small number changes
    ALDASH-511 small number changes

---
+++ plugins/wp-react-blocks-plugin/blocks/small-number/index.js
@@ -0,0 +1,101 @@
+import {__} from '@wordpress/i18n';
+import {registerBlockType} from '@wordpress/blocks';
+import BlockSave from "./BlockSave";
+import BlockEdit from "./BlockEdit";
+import {Generic} from '../icons/index.js'
+// HOC for core/paragraph removed; behavior moved to separate block `paragraph-variables`.
+
+registerBlockType(process.env.BLOCKS_NS + '/small-number',
+    {
+        title: __('Small Number'),
+        icon: Generic,
+        category: process.env.BLOCKS_CATEGORY,
+        apiVersion: 2,
+        supports: {
+            __experimentalInline: true,
+            html: false
+        },
+        attributes: {
+            type: {
+                type: 'string',
+                default: "big-number",
+            },
+            group: {
+                type: 'string',
+                default: 'default',
+            },
+            panelStatus: {
+                type: 'object',
+                default: {}
+            },
+            height: {
+                type: 'Numeric',
+                default: 120,
+            },
+            app: {
+                type: 'string',
+                default: "csv"
+            },
+            csv: {
+                type: 'string',
+                default: "Amount \n20000"
+            },    
+            params: {
+                type: 'object',
+                default: {}
+            },
+            format: {
+                type: 'object',
+                default: {
+                    "style": "decimal",
+                    "minimumFractionDigits": 0,
+                    "maximumFractionDigits": 0,
+                    "currency": "USD"
+                }
+            },
+            measures: {
+                type: 'array',
+                default: []
+            },
+            _measures: {},
+            filters: {
+                type: 'array',
+                default: []
+            },
+            dvzProxyDatasetId: {
+                type: 'string',
+                default: ""
+            },
+            types: {
+                type: 'array',
+                default: [
+                  {label: 'Big Number', value: 'big-number', supports: {singleMeasure: true, singleDimension: false}}
+                ]
+            },           
+            numberFontSize: {
+                type: 'Numeric',
+                default: 24
+            },
+            numberColor: {
+                type: 'string',
+                default: "#5a5d68",
+            },
+            waitForFilters: {
+                type: 'boolean',
+                default: false
+            },
+            noDataText: {
+                type: 'string',
+                default: '-'
+            },
+            textTemplate: {
+                type: 'string',
+                default: '{{value}}'
+            }
+        },
+
+        edit: BlockEdit,
+        save: BlockSave,
+    }
+);
+
