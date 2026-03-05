04555e985b7c4f7ed441a9e9bd2fa1319a5b3dbd
Sebastian Dimunzio <sdimunzio@gmail.com>
2026-03-05 12:34:12 -0300

    restoring filters

--- plugins/wp-react-blocks-plugin/blocks/downloads/index.js
+++ plugins/wp-react-blocks-plugin/blocks/downloads/index.js
@@ -1,106 +1,136 @@
-import {__} from '@wordpress/i18n';
-import {registerBlockType} from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
+import { registerBlockType } from '@wordpress/blocks';
+import BlockSaveV1 from "./BlockSavev1"
 import BlockSave from "./BlockSave";
 import BlockEdit from "./BlockEdit";
 import {GenericIcon, BLOCKS_NS, BLOCKS_CATEGORY} from '@devgateway/dvz-wp-commons';
+
+
+const oldAttributes = {
+    defaultFormat: {
+        type: 'string',
+        default: 'PNG'
+    },
+    title: {
+        type: 'string',
+        default: "Set your chart download file type"
+    },
+    useTitle: {
+        type: 'Boolean',
+        default: false,
+    },
+    style: {
+        type: 'String',
+        default: "heavy",
+    },
+    sectionTitle: {
+        type: 'string',
+        default: ""
+    },
+
+    buttonLabel: {
+        type: 'string',
+        default: "Download"
+    },
+    pngLabel: {
+        type: 'string',
+        default: 'export.png'
+    },
+    jpgLabel: {
+        type: 'string',
+        default: 'export.jpg'
+    },
+    pngText: {
+        type: 'string',
+        default: 'Download PNG'
+    },
+    jpgText: {
+        type: 'string',
+        default: 'Download JPG'
+    },
+    checkPng: {
+        type: 'boolean',
+        default: true
+    },
+    checkJpg: {
+        type: 'boolean',
+        default: false
+    },
+    height: {
+        type: 'Numeric',
+        default: 200,
+    },
+    fontSize: {
+        type: 'string',
+        default: '24px',
+    },
+    fontClass: {
+        type: 'string',
+        default: '24px',
+    },
+    color: {
+        type: 'string',
+        default: '24px',
+    },
+    downloadTooltip: {
+        type: 'string',
+        default: "Click to select download format"
+    },
+    tooltip: {
+        type: 'string',
+        default: "Click to select download format"
+    },
+    includeSourceURL: {
+        type: 'boolean',
+        default: false
+    },
+
+    sourceURLMarginLeft: {
+        type: 'Numeric',
+        default: 70
+    },
+    sourceURLMarginTop: {
+        type: 'Numeric',
+        default: 10
+    },
+    sourceURLFontSize: {
+        type: 'Numeric',
+        default: 18,
+    }
+}
+
+const deprecated = [
+    {
+        attributes: oldAttributes,
+
+        migrate(attrs) {
+            return {
+                ...attrs,
+                includeFilters: false
+            };
+        },
+
+        save: BlockSaveV1,
+    },
+];
+
+
+const newAttributes = {
+    ...oldAttributes,
+    includeFilters: {
+        type: 'boolean',
+        default: false
+    }
+}
 
 registerBlockType(BLOCKS_NS + '/download',
     {
         title: __('Download'),
         icon: GenericIcon,
         category: BLOCKS_CATEGORY,
-        attributes: {
-            defaultFormat: {
-                type: 'string',
-                default: 'PNG'
-            },
-            title: {
-                type: 'string',
-                default: "Set your chart download file type"
-            },
-            useTitle: {
-                type: 'Boolean',
-                default: false,
-            },
-            style: {
-                type: 'String',
-                default: "heavy",
-            },
-            sectionTitle: {
-                type: 'string',
-                default: ""
-            },
-
-            buttonLabel: {
-                type: 'string',
-                default: "Download"
-            },
-            pngLabel: {
-                type: 'string',
-                default: 'export.png'
-            },
-            jpgLabel: {
-                type: 'string',
-                default: 'export.jpg'
-            },
-            pngText: {
-                type: 'string',
-                default: 'Download PNG'
-            },
-            jpgText: {
-                type: 'string',
-                default: 'Download JPG'
-            },
-            checkPng: {
-                type: 'boolean',
-                default: true
-            },
-            checkJpg: {
-                type: 'boolean',
-                default: false
-            },
-            height: {
-                type: 'Numeric',
-                default: 200,
-            },
-            fontSize: {
-                type: 'string',
-                default: '24px',
-            },
-            fontClass: {
-                type: 'string',
-                default: '24px',
-            },
-            color: {
-                type: 'string',
-                default: '24px',
-            },
-            downloadTooltip: {
-                type: 'string',
-                default: "Click to select download format"
-            },
-            tooltip: {
-                type: 'string',
-                default: "Click to select download format"
-            },
-            includeSourceURL: {
-                type: 'boolean',
-                default: false
-            },
-            sourceURLMarginLeft: {
-                type: 'Numeric',
-                default: 70
-            },
-            sourceURLMarginTop: {
-                type: 'Numeric',
-                default: 10
-            },
-            sourceURLFontSize: {
-                type: 'Numeric',
-                default: 18,
-            }
-        },
+        attributes: newAttributes,
         edit: BlockEdit,
         save: BlockSave,
+        deprecated
     }
 );
