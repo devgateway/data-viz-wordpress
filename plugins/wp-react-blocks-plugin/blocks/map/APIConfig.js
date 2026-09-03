f2980e835d15c1fec595aa345a286729d42cf778
Timothy Mugo <tmugo@developmentgateway.org>
2026-09-03 12:02:21 +0300

    chore: check for potentially missing object keys before referencing them

--- plugins/wp-react-blocks-plugin/blocks/map/APIConfig.js
+++ plugins/wp-react-blocks-plugin/blocks/map/APIConfig.js
@@ -136,13 +136,13 @@ export class APIConfig extends Component {
         const {attributes: {type: prevType, dimension2:prevDimension2}} = prevProps
 
 
-        const prevTypeObject = types.filter(t => t.value === prevType).length > 0 ? types.filter(t => t.value === prevType)[0] : null
+        const prevTypeObject = types && types.filter(t => t.value === prevType).length > 0 ? types.filter(t => t.value === prevType)[0] : null
         const currentType = types && types.length > 0
             ? types.find(t => t.value === type)
             : null
 
         if (type != prevType && currentType) {
-            if (prevTypeObject.supports) {
+            if (prevTypeObject && prevTypeObject.supports) {
                 if (prevTypeObject.supports.singleMeasure != currentType.supports.singleMeasure || (currentType.supports.singleMeasure == false && dimension2 != "none")) {
                     setAttributes({measures: [], filters: []})
                 } else {
